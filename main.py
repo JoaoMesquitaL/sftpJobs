1e30cf67d8418bd0f98d69ad7635f990162caefc
JoaoMesquitaL <joao.pedroml@protonmail.com>
2026-05-25 19:32:18 -0300

    feat: bloco de execução de comando no SFTP

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ load_dotenv()
 
 client = paramiko.SSHClient()
 
+##Conexão com Servidor SFTP
 try:
     client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     client.connect(
@@ -15,7 +16,29 @@ try:
         password = os.getenv('sftpPass'))
 
     print("Sucesso!\n")
+
+    ##Execução de comando no servidor SFTP
+    try:
+
+        stdin, stdout, stderr = client.exec_command("cd sftpServer && dir")
+
+        resultado = stdout.read().decode('latin-1')
+        erro = stderr.read().decode('latin-1')
+
+        ##Essa estrutura permite a exibição do objeto de erro do exec_command
+        if erro:
+            print("Erro:")
+            print(erro)
+        else:
+            print("Resultado:")
+            print(resultado)
+
+    #Tratamento Erro: Execução de comando no servidor SFTP
+    except Exception as e:
+        print(f"Erro ao executar comando: {e}")
+    
     client.close()
 
+##Tratamento Erro: Conexão com Servidor SFTP
 except Exception as e:
     print(f"Erro ao conectar: {e}")
