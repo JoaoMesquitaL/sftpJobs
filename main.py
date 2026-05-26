42d7e29735003754c4e008ab01f1659d9552b75a
JoaoMesquitaL <joao.pedroml@protonmail.com>
2026-05-26 15:48:22 -0300

    feat: construtor da classe e refatoração da main

--- main.py
+++ main.py
@@ -1,41 +1,15 @@
-import paramiko 
 import os 
 from dotenv import load_dotenv
 
-from services import sftp_service  
+from services.sftp_service import SFTPService
 
 load_dotenv()
 
-client = paramiko.SSHClient()
+#Instanciada classe do SFTP Services
+client = SFTPService( 
+    host=os.getenv('sftpServer'),
+    port=os.getenv('sftpPort'),
+    user=os.getenv('sftpUser'),
+    password=os.getenv('sftpPass')
+)
 
-##Conexão com Servidor SFTP
-try:
-    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    client.connect(
-        hostname = os.getenv('sftpServer'), 
-        port = os.getenv('sftpPort'), 
-        username = os.getenv('sftpUser'), 
-        password = os.getenv('sftpPass'))
-
-    print("Sucesso!\n")
-
-    ##Execução de comando no servidor SFTP
-    try:
-        filePath = os.getenv('filePathLocal')
-        remotePath = os.getenv('filePathRemote')
-
-        sftp = client.open_sftp()
-        sftp.put(filePath, remotePath)
-
-        print("Arquivo enviado com sucesso!")
-        sftp.close()
-
-    #Tratamento Erro: Execução de comando no servidor SFTP
-    except Exception as e:
-        print(f"Erro ao executar comando: {e}")
-
-    client.close()
-
-##Tratamento Erro: Conexão com Servidor SFTP
-except Exception as e:
-    print(f"Erro ao conectar: {e}")
--- services/sftp_service.py
+++ services/sftp_service.py
@@ -1,3 +1,44 @@
 import paramiko
 
 class SFTPService: 
+
+    def __init__(self, host, port, user, password):
+        self.host = host
+        self.port = int(port)
+        self.user = user
+        self.password = password
+    
+
+##Conexão com Servidor SFTP
+#try:
+#    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+#    client.connect(
+#        hostname = os.getenv('sftpServer'), 
+#        port = os.getenv('sftpPort'), 
+#        username = os.getenv('sftpUser'), 
+#        password = os.getenv('sftpPass'))
+#
+#    print("Sucesso!\n")
+#
+#    ##Execução de comando no servidor SFTP
+#    try:
+#        filePath = os.getenv('filePathLocal')
+#        remotePath = os.getenv('filePathRemote')
+#
+#        sftp = client.open_sftp()
+#        sftp.put(filePath, remotePath)
+#
+#        print("Arquivo enviado com sucesso!")
+#        sftp.close()
+#
+#    #Tratamento Erro: Execução de comando no servidor SFTP
+#    except Exception as e:
+#        print(f"Erro ao executar comando: {e}")
+#
+#    client.close()
+
+##Tratamento Erro: Conexão com Servidor SFTP
+#except Exception as e:
+#    print(f"Erro ao conectar: {e}")
+
+
